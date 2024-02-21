6507805434712226c70ed13c78be36745f787a1e
mllangon <mllangon@myuax.com>
2024-02-21 11:18:11 +0100

    2

--- src/main/java/cronometroCosmico.java
+++ src/main/java/cronometroCosmico.java
@@ -1,2 +1,7 @@
 public class cronometroCosmico {
-}
+
+    private static final double HORAS_POR_DIA_PLANETA = 30.0;
+    private static final double DIAS_POR_AÑO_PLANETA = 500.0;
+    private static final double HORAS_POR_DIA_TIERRA = 24.0;
+    private static final double DIAS_POR_AÑO_TIERRA = 365.25;
+
